c0c2557e1423144bed57f52fccaf0da14a299c79
Darival <cesarrcv1992@hotmail.com>
2015-01-06 04:34:44 -0600

    LOLS

---
+++ SerialGPSGoogleMAPS/Form1.Designer.cs
@@ -0,0 +1,255 @@
+﻿namespace SerialGPSGoogleMAPS
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variable del diseñador requerida.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén utilizando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido del método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.splitContainer1 = new System.Windows.Forms.SplitContainer();
+            this.btn_conectar = new System.Windows.Forms.Button();
+            this.webBrowser1 = new System.Windows.Forms.WebBrowser();
+            this.tb_latitud = new System.Windows.Forms.TextBox();
+            this.tb_longitud = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.serialPort1 = new System.IO.Ports.SerialPort(this.components);
+            this.tb_serialPort = new System.Windows.Forms.RichTextBox();
+            this.btn_reset = new System.Windows.Forms.Button();
+            this.tb_speed = new System.Windows.Forms.TextBox();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.cb_port = new System.Windows.Forms.ComboBox();
+            ((System.ComponentModel.ISupportInitialize)(this.splitContainer1)).BeginInit();
+            this.splitContainer1.Panel1.SuspendLayout();
+            this.splitContainer1.Panel2.SuspendLayout();
+            this.splitContainer1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // splitContainer1
+            // 
+            this.splitContainer1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.splitContainer1.Location = new System.Drawing.Point(0, 0);
+            this.splitContainer1.Name = "splitContainer1";
+            // 
+            // splitContainer1.Panel1
+            // 
+            this.splitContainer1.Panel1.Controls.Add(this.cb_port);
+            this.splitContainer1.Panel1.Controls.Add(this.label5);
+            this.splitContainer1.Panel1.Controls.Add(this.label4);
+            this.splitContainer1.Panel1.Controls.Add(this.tb_speed);
+            this.splitContainer1.Panel1.Controls.Add(this.btn_reset);
+            this.splitContainer1.Panel1.Controls.Add(this.tb_serialPort);
+            this.splitContainer1.Panel1.Controls.Add(this.label3);
+            this.splitContainer1.Panel1.Controls.Add(this.label2);
+            this.splitContainer1.Panel1.Controls.Add(this.label1);
+            this.splitContainer1.Panel1.Controls.Add(this.tb_longitud);
+            this.splitContainer1.Panel1.Controls.Add(this.tb_latitud);
+            this.splitContainer1.Panel1.Controls.Add(this.btn_conectar);
+            // 
+            // splitContainer1.Panel2
+            // 
+            this.splitContainer1.Panel2.Controls.Add(this.webBrowser1);
+            this.splitContainer1.Size = new System.Drawing.Size(835, 357);
+            this.splitContainer1.SplitterDistance = 278;
+            this.splitContainer1.TabIndex = 0;
+            // 
+            // btn_conectar
+            // 
+            this.btn_conectar.Location = new System.Drawing.Point(171, 10);
+            this.btn_conectar.Name = "btn_conectar";
+            this.btn_conectar.Size = new System.Drawing.Size(75, 23);
+            this.btn_conectar.TabIndex = 0;
+            this.btn_conectar.Text = "&Connect";
+            this.btn_conectar.UseVisualStyleBackColor = true;
+            this.btn_conectar.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // webBrowser1
+            // 
+            this.webBrowser1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.webBrowser1.Location = new System.Drawing.Point(0, 0);
+            this.webBrowser1.MinimumSize = new System.Drawing.Size(20, 20);
+            this.webBrowser1.Name = "webBrowser1";
+            this.webBrowser1.Size = new System.Drawing.Size(553, 357);
+            this.webBrowser1.TabIndex = 0;
+            // 
+            // tb_latitud
+            // 
+            this.tb_latitud.Location = new System.Drawing.Point(67, 39);
+            this.tb_latitud.Name = "tb_latitud";
+            this.tb_latitud.Size = new System.Drawing.Size(100, 20);
+            this.tb_latitud.TabIndex = 2;
+            // 
+            // tb_longitud
+            // 
+            this.tb_longitud.Location = new System.Drawing.Point(67, 65);
+            this.tb_longitud.Name = "tb_longitud";
+            this.tb_longitud.Size = new System.Drawing.Size(100, 20);
+            this.tb_longitud.TabIndex = 3;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(13, 42);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(48, 13);
+            this.label1.TabIndex = 4;
+            this.label1.Text = "Latitude:";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(4, 68);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(57, 13);
+            this.label2.TabIndex = 5;
+            this.label2.Text = "Longitude:";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(32, 15);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(29, 13);
+            this.label3.TabIndex = 6;
+            this.label3.Text = "Port:";
+            // 
+            // serialPort1
+            // 
+            this.serialPort1.BaudRate = 115200;
+            this.serialPort1.PortName = "COM4";
+            this.serialPort1.DataReceived += new System.IO.Ports.SerialDataReceivedEventHandler(this.serialPort1_DataReceived);
+            // 
+            // tb_serialPort
+            // 
+            this.tb_serialPort.Location = new System.Drawing.Point(12, 122);
+            this.tb_serialPort.Name = "tb_serialPort";
+            this.tb_serialPort.Size = new System.Drawing.Size(248, 130);
+            this.tb_serialPort.TabIndex = 7;
+            this.tb_serialPort.Text = "";
+            // 
+            // btn_reset
+            // 
+            this.btn_reset.Location = new System.Drawing.Point(13, 259);
+            this.btn_reset.Name = "btn_reset";
+            this.btn_reset.Size = new System.Drawing.Size(75, 23);
+            this.btn_reset.TabIndex = 8;
+            this.btn_reset.Text = "Reset";
+            this.btn_reset.UseVisualStyleBackColor = true;
+            this.btn_reset.Click += new System.EventHandler(this.btn_reset_Click);
+            // 
+            // tb_speed
+            // 
+            this.tb_speed.Location = new System.Drawing.Point(71, 96);
+            this.tb_speed.Name = "tb_speed";
+            this.tb_speed.Size = new System.Drawing.Size(61, 20);
+            this.tb_speed.TabIndex = 9;
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(24, 99);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(41, 13);
+            this.label4.TabIndex = 10;
+            this.label4.Text = "Speed:";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(138, 99);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(32, 13);
+            this.label5.TabIndex = 11;
+            this.label5.Text = "km/h";
+            // 
+            // cb_port
+            // 
+            this.cb_port.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+            this.cb_port.FormattingEnabled = true;
+            this.cb_port.Items.AddRange(new object[] {
+            "COM",
+            "COM",
+            "COM",
+            "COM1",
+            "COM2",
+            "COM3",
+            "COM4",
+            "COM5",
+            "COM6",
+            "COM7",
+            "COM8",
+            "COM9",
+            "COM10",
+            "COM11",
+            "COM12",
+            "COM13",
+            "COM14"});
+            this.cb_port.Location = new System.Drawing.Point(67, 12);
+            this.cb_port.Name = "cb_port";
+            this.cb_port.Size = new System.Drawing.Size(74, 21);
+            this.cb_port.TabIndex = 12;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(835, 357);
+            this.Controls.Add(this.splitContainer1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.FormClosed += new System.Windows.Forms.FormClosedEventHandler(this.Form1_FormClosed);
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.splitContainer1.Panel1.ResumeLayout(false);
+            this.splitContainer1.Panel1.PerformLayout();
+            this.splitContainer1.Panel2.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.splitContainer1)).EndInit();
+            this.splitContainer1.ResumeLayout(false);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.SplitContainer splitContainer1;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox tb_longitud;
+        private System.Windows.Forms.TextBox tb_latitud;
+        private System.Windows.Forms.Button btn_conectar;
+        private System.Windows.Forms.WebBrowser webBrowser1;
+        private System.IO.Ports.SerialPort serialPort1;
+        private System.Windows.Forms.RichTextBox tb_serialPort;
+        private System.Windows.Forms.Button btn_reset;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.TextBox tb_speed;
+        private System.Windows.Forms.ComboBox cb_port;
+    }
+}
+
---
+++ SerialGPSGoogleMAPS/Form1.cs
@@ -0,0 +1,100 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.IO.Ports;
+
+namespace SerialGPSGoogleMAPS
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        string RXmssg = System.String.Empty;
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            serialPort1.PortName = cb_port.SelectedItem.ToString();
+            try
+            {
+                if (!serialPort1.IsOpen)
+                {
+                    tb_serialPort.Text = serialPort1.PortName + " Listo!\r\n";
+                    serialPort1.Open();
+                    btn_conectar.Enabled = false;
+                }
+                else
+                {
+                    tb_serialPort.Text="ya esta abierto";
+                }
+            }
+            catch (UnauthorizedAccessException ex)
+            {
+                MessageBox.Show(ex.Message, "Port might be already Open or Port does not exist");
+                throw;
+            }
+        }
+
+        private void btn_reset_Click(object sender, EventArgs e)
+        {
+            tb_serialPort.Clear();
+        }
+
+        private void serialPort1_DataReceived(object sender, System.IO.Ports.SerialDataReceivedEventArgs e)
+        {
+            RXmssg = serialPort1.ReadLine();
+            if (RXmssg!=string.Empty)
+            {
+            this.Invoke(new EventHandler(desplegar));
+            }
+        }
+
+        private void desplegar(object sender, EventArgs e)
+        {
+            tb_serialPort.AppendText(RXmssg);
+            string[] cadena = RXmssg.Split(';');
+            int numero = cadena.Length;
+            tb_speed.Text = cadena[4];
+            string latidude = cadena[2];
+            tb_latitud.Text = latidude;
+            string longitude = cadena[3];
+            tb_longitud.Text = longitude;
+            try
+            {
+                StringBuilder tracker = new StringBuilder();
+                tracker.Append("http://maps.google.com/maps?q=");
+                if (longitude!=string.Empty&&latidude!=string.Empty)
+                {
+                    tracker.Append(latidude+","+longitude);
+                    webBrowser1.Navigate(tracker.ToString());
+                }
+                else
+                {
+                    MessageBox.Show("error");
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message,"error");
+                throw;
+            }
+        }
+        private void Form1_FormClosed(object sender, FormClosedEventArgs e)
+        {
+            serialPort1.Close();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            cb_port.DataSource = SerialPort.GetPortNames();
+        }
+    }
+}
